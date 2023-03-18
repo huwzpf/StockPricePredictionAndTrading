a39b1e4b63672d0dc6ca2aba016441fb21a8454b
Piotr Sieński <s184297@student.pg.edu.pl>
2023-03-18 14:22:00 +0100

    little AR refactor

--- statistical/AR.py
+++ statistical/AR.py
@@ -1,11 +1,34 @@
+import pandas
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 from statsmodels.graphics.tsaplots import pacf
+from typing import List
 
 '''
 autoregressive model 
 '''
+
+
+def AR(q: int, data_vector: pandas.DataFrame, zero_coeffs: List[int] = None) -> np.ndarray:
+    # get coefficients for linear regression using PACF (coeffs * X + b = Y)
+    data_pacf = pacf(data_vector)
+    coeffs = data_pacf[:q]
+    if zero_coeffs:
+        for zero_coeff in zero_coeffs:
+            assert(zero_coeff < q)
+            coeffs[zero_coeff] = 0
+
+    # b = mean(coeffs * X - Y)
+    res = data_vector.rolling(q).apply(lambda x: x.dot(coeffs))
+    res_trimmed = res[q-1:-1].to_numpy()
+    y = data_vector[q:].to_numpy()
+    b = np.mean(y - res_trimmed)
+
+    # calculate predictions
+    preds = data_vector.rolling(q).apply(lambda x: x.dot(coeffs) + b)
+    return preds[q:].to_numpy()
+
 
 if __name__ == "__main__":
     # prepare data
@@ -14,24 +37,9 @@ if __name__ == "__main__":
     data = pd.read_csv(data_dir).iloc[:, column]
     data_vector = (data - np.mean(data))/np.std(data)
 
-    # get coefficients for linear regression using PACF (coeffs * X + b = Y)
-    data_pacf = pacf(data_vector)
-    coeffs = data_pacf[:5]
-
-    # print(coeffs)
-    # [ 1.          0.99510463 -0.0209996   0.03668808 -0.02132917]
-    # first coefficient is discarded
-
-    coeffs[:1] = [0]
-
-    # b = mean(coeffs * X - Y)
-    res = data_vector.rolling(5).apply(lambda x: x.dot(coeffs))
-    res_trimmed = res[4:-1].to_numpy()
-    y = data_vector[5:].to_numpy()
-    b = np.mean(y - res_trimmed)
-
-    # calculate predictions
-    preds = data_vector.rolling(5).apply(lambda x: x.dot(coeffs) + b)[5:].to_numpy()
+    q = 3
+    y = data_vector[q:].to_numpy()
+    preds = AR(q, data_vector, [0])
 
     # plot predictions
     plt.plot(preds, c="r")
